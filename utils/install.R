9da286a91ac74d5813a74f2de3f45903b51a28c0
sudorook <daemon@nullcodon.com>
2020-11-27 20:01:57 -0600

    add wrapper script for installing r packages

---
+++ utils/install.R
@@ -0,0 +1,6 @@
+#! /usr/bin/env Rscript
+
+install.packages(c("ggplot2", "tidyr", "tibble", "dplyr", "data.table",
+                   "mvtnorm", "quadprog", "expm", "RcppArmadillo", "RcppEigen",
+                   "bipartite", "vegan", "IRkernel", "devtools", "knitr",
+                   "latex2exp", "MASS", "RColorBrewer", "reshape2", "rvest"))
